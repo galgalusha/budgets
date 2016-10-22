5700280b95c31377ecc0152e6651d3687c4dbaa8
galgalusha <koren.gal@gmail.com>
2016-10-22 18:19:58 +0300

    real impl to NewExpenseService

--- business/src/main/java/galko/budgets/business/api/web/services/NewExpenseService.java
+++ business/src/main/java/galko/budgets/business/api/web/services/NewExpenseService.java
@@ -3,6 +3,10 @@ package galko.budgets.business.api.web.services;
 import galko.budgets.business.api.web.IService;
 import galko.budgets.business.api.web.dto.NewExpenseRequest;
 import galko.budgets.business.api.web.dto.TextMessage;
+import galko.budgets.business.model.Budget;
+import galko.budgets.business.model.tinytypes.BillAmount;
+import galko.budgets.persistency.api.dto.BudgetDbo;
+import galko.budgets.persistency.api.query.IBudgetDba;
 import galko.service_locator.ServiceLocator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -11,13 +15,23 @@ public class NewExpenseService implements IService<NewExpenseRequest, TextMessag
 
     private final static Logger logger = LogManager.getLogger(NewExpenseService.class);
 
+    private final IBudgetDba budgetDba;
+    private final ServiceLocator serviceLocator;
+
     public NewExpenseService(ServiceLocator serviceLocator) {
+        this.serviceLocator = serviceLocator;
+        budgetDba = serviceLocator.resolve(IBudgetDba.class);
     }
 
     public TextMessage handle(NewExpenseRequest request) {
-        logger.info("----------- request --------------");
-        logger.info("budget id: " + request.getBudgetId());
-        logger.info("amount: " + request.getAmount());
+
+        final BudgetDbo budgetDbo = budgetDba.getById(request.getBudgetId()).get();
+
+        final Budget budget = new Budget(serviceLocator, budgetDbo);
+
+        budget.getActiveBill().addExpense(BillAmount.of(request.getAmount()));
+
+        logger.info("Added expense of " + request.getAmount() + " to budget " + request.getBudgetId() + " of user " + request.getUserId().get());
 
         return new TextMessage("Saved Successfully");
     }
